8cd6e3192056841af174b3a1ca2e42dbc6d33bff
arjit-1 <38866263+arjit-1@users.noreply.github.com>
2021-03-16 11:48:58 +0530

    Add files via upload

---
+++ src/main/java/com/kuliza/demo/repository/dynamicUserTestingLogsRepository.java
@@ -0,0 +1,11 @@
+package com.kuliza.demo.repository;
+
+import com.kuliza.demo.model.dynamicUserTestingLogs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface dynamicUserTestingLogsRepository extends JpaRepository<dynamicUserTestingLogs,Long> {
+
+
+
+
+}
